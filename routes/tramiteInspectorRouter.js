1236634347b2552d1c907728c4ef251df1dc39ab
Vieri1 <vieriflores2002xdxd@correo.com>
2024-12-05 15:27:57 -0500

    asd

--- routes/tramiteInspectorRouter.js
+++ routes/tramiteInspectorRouter.js
@@ -3,7 +3,6 @@ const router = Router();
 const { uploadDocuments }  = require('../middlewares/uploadMiddleware');
 
 const { createTramiteHandler, allTramiteHandler } = require('../handlers/tramiteInspectorHandler');
-const permisoAutorizacion = require("../checkers/roleAuth");
 
 router.post('/nuevoTramiteNC', uploadDocuments, createTramiteHandler);
 router.get('/misTramites/:id', allTramiteHandler);
