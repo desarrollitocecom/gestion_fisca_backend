ce8cd513b985d9d3ee5742a92576302e7389d5e4
Vieri1 <vieriflores2002xdxd@correo.com>
2024-12-05 15:36:40 -0500

    Revert "asd"
    This reverts commit 1236634347b2552d1c907728c4ef251df1dc39ab.

--- routes/tramiteInspectorRouter.js
+++ routes/tramiteInspectorRouter.js
@@ -3,6 +3,7 @@ const router = Router();
 const { uploadDocuments }  = require('../middlewares/uploadMiddleware');
 
 const { createTramiteHandler, allTramiteHandler } = require('../handlers/tramiteInspectorHandler');
+const permisoAutorizacion = require("../checkers/roleAuth");
 
 router.post('/nuevoTramiteNC', uploadDocuments, createTramiteHandler);
 router.get('/misTramites/:id', allTramiteHandler);
