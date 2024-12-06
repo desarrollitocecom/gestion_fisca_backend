7913d4043c0632a3785e6c430264d86009910c41
Vieri1 <vieriflores2002xdxd@correo.com>
2024-12-06 08:35:48 -0500

    a

--- routes/rsgpRouter.js
+++ routes/rsgpRouter.js
@@ -1,19 +1,14 @@
 const {Router}=require ('express');
 const router = Router();
-const {uploadRSGP,uploadNone}=require('../middlewares/uploadMiddleware')
+const {uploadRSGP}=require('../middlewares/uploadMiddleware')
 
 const {
     createRsgpHandler,
-    updateRsgpHandler,
-    getRsgpHandler,
-    getAllRsgpHandler
-
 }=require('../handlers/rsgpHandler');
 const permisoAutorizacion = require("../checkers/roleAuth");
 
 
 router.patch("/:id",permisoAutorizacion(["all_system_access", "create_rsgp"]),uploadRSGP,createRsgpHandler);
-//router.patch("/:id",permisoAutorizacion(["all_system_access", "update_rsgp"]),uploadRSGP,updateRsgpHandler);
-//router.post("/modiRGSP",permisoAutorizacion(["all_system_access", "update_rsgp"]),uploadNone,updateinRsaHandler);
+
 
 module.exports = router;
