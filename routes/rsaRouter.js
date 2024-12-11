c08fec82bbb5d9541c8bf4a017a42ddbf2b1c91b
Vieri1 <vieriflores2002xdxd@correo.com>
2024-12-11 14:11:30 -0500

    abc

--- routes/rsaRouter.js
+++ routes/rsaRouter.js
@@ -4,14 +4,15 @@ const {uploadRSA}=require('../middlewares/uploadMiddleware')
 
 const {
     createRsaHandler,
-    
+    getAllRSAforAR3Handler,
     getRsaHandler,
-    getAllRsaHandler,
+    getAllRsaHandler
    
     
 }=require('../handlers/rsaHandler');
 const permisoAutorizacion = require("../checkers/roleAuth");
 
+router.get("/ifi_for_AreaResolutiva3",permisoAutorizacion(["all_system_access", "read_rsa"]), getAllRSAforAR3Handler);
 router.get("/",permisoAutorizacion(["all_system_access", "read_rsa"]),getAllRsaHandler);
 router.get('/:id',permisoAutorizacion(["all_system_access", "read_rsa"]),getRsaHandler);
 router.patch("/:id",permisoAutorizacion(["all_system_access", "create_rsa"]),uploadRSA,createRsaHandler);
