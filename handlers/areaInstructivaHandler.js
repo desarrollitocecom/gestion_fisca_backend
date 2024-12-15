9e30868d8ef3092edbaa1e9842ea3ceae543caf9
soportels <soportels@gmail.com>
2024-12-15 13:07:52 -0500

    documento

--- handlers/areaInstructivaHandler.js
+++ handlers/areaInstructivaHandler.js
@@ -1,5 +1,7 @@
 const { updateNC, getAllNCforInstructiva } = require('../controllers/ncController');
 const { createInformeFinalController } = require('../controllers/informeFinalController');
+const { updateDocumento }=require('../controllers/documentoController');
+
 
 function isValidUUID(uuid) {
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
