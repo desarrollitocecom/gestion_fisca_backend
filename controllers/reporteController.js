58b446cd0ec4c5579d4f022ff9e22f577b13fe62
soportels <soportels@gmail.com>
2024-12-20 23:24:46 -0500

    para el reporte

--- controllers/reporteController.js
+++ controllers/reporteController.js
@@ -1,15 +1,19 @@
 const { RG, Usuario,  NC , TramiteInspector } = require('../db_connection'); 
 const { Sequelize } = require('sequelize');
-const { RSG1, DescargoNC, DescargoRSG, IFI, DescargoIFI, RSG2, RSA, DescargoRSA, RSG, Acta } = require('../db_connection'); 
+const { RSG1, DescargoNC, DescargoRSG, IFI, Entidad, DescargoIFI, RSG2, RSA, DescargoRSA, RSG, Acta } = require('../db_connection'); 
 
 const getAllDataController = async () => {
     try {
         const response = await NC.findAll({ 
             order: [['id', 'ASC']],
             attributes: [
-                'id',
-                [Sequelize.col('tramiteInspector.nro_nc'), 'nro_nc'],
-                [Sequelize.col('tramiteInspector.inspectorUsuario.usuario'), 'usuarioInspector'],
+                'id','ordenanza_municipal','nro_documento', 'lugar_infraccion', 'observaciones', 
+                [Sequelize.col('entidad.nombre_entidad'), 'razon_social'],
+                [Sequelize.col('tramiteInspector.nro_nc'), 'numero_nc'],
+                [Sequelize.col('tramiteInspector.nro_acta'), 'numero_acta'],
+                [Sequelize.col('tramiteInspector.createdAt'), 'fecha_NC'],
+
+                [Sequelize.col('tramiteInspector.inspectorUsuario.usuario'), 'nombre_inspector'],
                 [Sequelize.literal(`'NOTIFICACION DE CARGO'`), 'nombre_nc'],
                 [Sequelize.col('tramiteInspector.documento_nc'), 'documento_nc'],
                 [Sequelize.literal(`'ACTA DE FISCALIZACION'`), 'nombre_acta'],
@@ -18,55 +22,88 @@ const getAllDataController = async () => {
 
                 [Sequelize.col('digitadorUsuario.usuario'), 'usuarioDigitador'],
 
+                //----------DESCARGO NC---------
                 [Sequelize.col('descargoNC.analistaUsuario.usuario'), 'usuarioAnalista1'],
                 [Sequelize.literal(`'DESCARGO NC'`), 'nombre_descargoNC'],
+                [Sequelize.col('descargoNC.nro_descargo'), 'numero_descargoNC'],//
+                [Sequelize.col('descargoNC.fecha_descargo'), 'fecha_descargoNC'],//
                 [Sequelize.col('descargoNC.documento'), 'documento_descargoNC'],
                 [Sequelize.col('descargoNC.createdAt'), 'analista_createdAt'],
 
+                //----------CREAR INFORME FINAL---------
                 [Sequelize.col('IFI.Usuarios.usuario'), 'usuarioAreaInstructiva1'],
                 [Sequelize.literal(`'INFORME FINAL'`), 'nombre_AI'],
+                [Sequelize.col('IFI.nro_ifi'), 'numero_IFI'],//
+                [Sequelize.col('IFI.fecha'), 'fecha_IFI'],//
                 [Sequelize.col('IFI.documento_ifi'), 'documento_AI'],
                 [Sequelize.col('IFI.createdAt'), 'AI_createdAt'],
 
                 //primera muerte
+                //----------------RESOLUCION SUBGERENCIAL 1-------------
                 [Sequelize.col('IFI.TERMINADO_RSG1.Usuarios.usuario'), 'usuarioRSG1'],
                 [Sequelize.literal(`'RESOLUCION SUBGERENCIAL 1'`), 'nombre_RSG1'],
+                [Sequelize.col('IFI.TERMINADO_RSG1.nro_resolucion'), 'numero_RSG1'],//
+                [Sequelize.col('IFI.TERMINADO_RSG1.fecha_resolucion'), 'fecha_resolucion'],//
                 [Sequelize.col('IFI.TERMINADO_RSG1.documento'), 'documento_RSG1'],
                 [Sequelize.col('IFI.TERMINADO_RSG1.createdAt'), 'RSG1_createdAt'],
 
+                //----------DESCARGO IFI---------
                 [Sequelize.col('IFI.DescargoIFIs.analista2Usuario.usuario'), 'usuarioAnalista2'],
                 [Sequelize.literal(`'DESCARGO IFI'`), 'nombre_DIFI'],
+                [Sequelize.col('IFI.DescargoIFIs.nro_descargo'), 'numero_DIFI'],//
+                [Sequelize.col('IFI.DescargoIFIs.fecha_descargo'), 'fecha_descargo'],//
                 [Sequelize.col('IFI.DescargoIFIs.documento_DIFI'), 'documento_DIFI'],
                 [Sequelize.col('IFI.DescargoIFIs.createdAt'), 'analista2_createdAt'],
 
-                //primera muerte
+                //segunda muerte
+                //----------------RESOLUCION SUBGERENCIAL 2-------------
                 [Sequelize.col('IFI.RSG2.Usuarios.usuario'), 'usuarioAreaInstructiva2'],
                 [Sequelize.literal(`'RESOLUCION SUBGERENCIAL 2'`), 'nombre_AR2'],
+                [Sequelize.col('IFI.RSG2.nro_resolucion2'), 'numero_AR2'],//
+                [Sequelize.col('IFI.RSG2.fecha_resolucion'), 'fecha_resolucion'],//
                 [Sequelize.col('IFI.RSG2.documento'), 'documento_AR2'],
                 [Sequelize.col('IFI.RSG2.createdAt'), 'AR2_createdAt'],
 
+                //----------------RESOLUCION SANCIONADORA ADMINISTRATIVA-------------
                 [Sequelize.col('IFI.RSA.Usuarios.usuario'), 'usuarioAreaInstructiva2'],
                 [Sequelize.literal(`'RESOLUCION SANCIONADORA ADMINISTRATIVA'`), 'nombre_RSA'],
+                [Sequelize.col('IFI.RSA.nro_rsa'), 'numero_RSA'],//
+                [Sequelize.col('IFI.RSA.fecha_rsa'), 'fecha_RSA'],//
+                [Sequelize.col('IFI.RSA.fecha_notificacion'), 'fecha_notificacion_RSA'],    //
                 [Sequelize.col('IFI.RSA.documento_RSA'), 'documento_RSA'],
                 [Sequelize.col('IFI.RSA.createdAt'), 'RSA_createdAt'],
 
+                //----------------DESCARGO RESOLUCION SANCIONADORA ADMINISTRATIVA-------------
                 [Sequelize.col('IFI.RSA.DescargoRSAs.Usuarios.usuario'), 'usuarioAnalista3'],
                 [Sequelize.literal(`'DESCARGO RSA'`), 'nombre_DRSA'],
+                [Sequelize.col('IFI.RSA.DescargoRSAs.nro_descargo'), 'numero_DRSA'],//
+                [Sequelize.col('IFI.RSA.DescargoRSAs.fecha_descargo'), 'fecha_DRSA'],//
                 [Sequelize.col('IFI.RSA.DescargoRSAs.documento_DRSA'), 'documento_DRSA'],
                 [Sequelize.col('IFI.RSA.DescargoRSAs.createdAt'), 'DRSA_createdAt'],
 
-                [Sequelize.col('IFI.RSA.RSGs.Usuarios.usuario'), 'usuarioAreaInstructiva3'],
+                //----------------RESOLUCION SUBGERENCIAL-------------
+                [Sequelize.col('IFI.RSA.RSGs.Usuarios.usuario'), 'usuarioAreaInstructiva3'], 
                 [Sequelize.literal(`'RESOLUCION SUBGERENCIAL GENERAL 3'`), 'nombre_RSG3'],
+                [Sequelize.col('IFI.RSA.RSGs.nro_rsg'), 'numero_RSG'],//
+                [Sequelize.col('IFI.RSA.RSGs.fecha_rsg'), 'fecha_RSG'],//
+                [Sequelize.col('IFI.RSA.RSGs.fecha_notificacion'), 'fecha_notificacion_RSG'],//
                 [Sequelize.col('IFI.RSA.RSGs.documento_RSG'), 'documento_RSG'],
                 [Sequelize.col('IFI.RSA.RSGs.createdAt'), 'RSG3_createdAt'],
 
+                //----------------DESCARGO RESOLUCION SUBGERENCIAL-------------
                 [Sequelize.col('IFI.RSA.RSGs.DescargoRSGs.Usuarios.usuario'), 'usuarioAnalista4'],
                 [Sequelize.literal(`'DESCARGO RSG'`), 'nombre_DRSG'],
+                [Sequelize.col('IFI.RSA.RSGs.DescargoRSGs.nro_descargo'), 'nro_RSG'],//
+                [Sequelize.col('IFI.RSA.RSGs.DescargoRSGs.fecha_descargo'), 'fecha_RSG'],//
                 [Sequelize.col('IFI.RSA.RSGs.DescargoRSGs.documento_DRSG'), 'documento_DRSG'],
                 [Sequelize.col('IFI.RSA.RSGs.DescargoRSGs.createdAt'), 'DRSG_createdAt'],
 
+                //----------------RESOLUCION GERENCIAL-------------
                 [Sequelize.col('IFI.RSA.RSGs.RGs.Usuarios.usuario'), 'usuarioGerencia'],
                 [Sequelize.literal(`'RESOLUCION GENERAL PROCEDENTE'`), 'nombre_RG'],
+                [Sequelize.col('IFI.RSA.RSGs.RGs.nro_rg'), 'numero_RG'],
+                [Sequelize.col('IFI.RSA.RSGs.RGs.fecha_rg'), 'fecha_RG'],
+                [Sequelize.col('IFI.RSA.RSGs.RGs.fecha_notificacion'), 'fecha_notificacion_RG'],
                 [Sequelize.col('IFI.RSA.RSGs.RGs.documento_rg'), 'documento_RG'],
                 [Sequelize.col('IFI.RSA.RSGs.RGs.createdAt'), 'RG_createdAt'],
 
@@ -86,6 +123,11 @@ const getAllDataController = async () => {
                             as: 'inspectorUsuario'
                         }
                     ],
+                    attributes: [], 
+                },
+                {
+                    model: Entidad, 
+                    as: 'entidad', 
                     attributes: [], 
                 },
                 {
@@ -230,86 +272,58 @@ const getAllDataController = async () => {
 
         const transformedData = response.map(row => ({
             id: row.get('id'),
-            nro_nc: row.get('nro_nc'),
-            etapaInspector: row.get('usuarioInspector') ? {
-                usuarioInspector: row.get('usuarioInspector'),
-                documento_nc: {
-                    nombre: row.get('nombre_nc'),
-                    path: row.get('documento_nc'),
-                },
-                documento_acta: {
-                    nombre: row.get('nombre_acta'),
-                    path: row.get('documento_acta'),
-                },
-                inspector_createdAt: row.get('inspector_createdAt'),
+            etapaNC: row.get('inspector_createdAt') ? {
+                ordenanza: row.get('ordenanza_municipal'),
+                razon_social: row.get('razon_social'),
+                nro_documento: row.get('nro_documento'),
+                numero_nc: row.get('numero_nc'),
+                numero_acta: row.get('numero_acta'),
+                fecha_NC: row.get('fecha_NC'),
+                lugar_infraccion: row.get('lugar_infraccion'),
+                observaciones: row.get('observaciones'),
+                nombre_inspector: row.get('nombre_inspector'),
             } : undefined,
             etapaDigitador: row.get('usuarioDigitador') ? {
                 usuarioDigitador: row.get('usuarioDigitador'),
                 digitador_createdAt: row.get('inspector_createdAt'),
             } : undefined,
             estapaDescargoNC: row.get('usuarioAnalista1') ? {
-                usuarioAnalista1: row.get('usuarioAnalista1'),
-                documento_descargoNC: {
-                    nombre: row.get('nombre_descargoNC'),
-                    path: row.get('documento_descargoNC')
-                },
-                analista_createdAt: row.get('analista_createdAt'),
+                numero_descargoNC: row.get('numero_descargoNC'),
+                fecha_descargoNC: row.get('fecha_descargoNC'),
             } : undefined,
             etapaAreaInstructiva: row.get('usuarioAreaInstructiva1') ? {
-                usuarioAreaInstructiva1: row.get('usuarioAreaInstructiva1'),
-                documento_AI: {
-                    nombre: row.get('nombre_AI'),
-                    path: row.get('documento_AI'),
-                },
-                AI_createdAt: row.get('AI_createdAt'),
+                numero_IFI: row.get('numero_IFI'),
+                fecha_IFI: row.get('fecha_IFI'),
+            } : undefined,
+            etapaRSG1: row.get('usuarioRSG1') ? {
+                fecha_resolucion_RSG1: row.get('fecha_resolucion'),
             } : undefined,
             etapaDescargoIFI: row.get('usuarioAnalista2') ? {
-                usuarioAnalista2: row.get('usuarioAnalista2'),
-                documento_descargoIFI: {
-                    nombre: row.get('nombre_DIFI'),
-                    path: row.get('documento_DIFI'),
-                },
-                analista2_createdAt: row.get('analista2_createdAt'),
+                numero_DIFI: row.get('numero_DIFI'),
+                fecha_descargo: row.get('fecha_descargo'),
             } : undefined,
             etapaRSA: row.get('usuarioAreaInstructiva2') ? {
-                usuarioAreaInstructiva2: row.get('usuarioAreaInstructiva2'),
-                documento_RSA: {
-                    nombre: row.get('nombre_RSA'),
-                    path: row.get('documento_RSA'),
-                },
-                AR2_createdAt: row.get('RSA_createdAt'),
+                numero_RSA: row.get('numero_RSA'),
+                fecha_RSA: row.get('fecha_RSA'),
+                fecha_notificacion_RSA: row.get('fecha_notificacion_RSA'),
             } : undefined,
             etapaDescargoRSA: row.get('usuarioAnalista3') ? {
-                usuarioAnalista3: row.get('usuarioAnalista3'),
-                documento_DRSA: {
-                    nombre: row.get('nombre_DRSA'),
-                    path: row.get('documento_DRSA'),
-                },
-                analista3_createdAt: row.get('DRSA_createdAt'),
+                numero_DRSA: row.get('numero_DRSA'),
+                fecha_DRSA: row.get('fecha_DRSA'),
             } : undefined,
             etapaRSG: row.get('usuarioAreaInstructiva3') ? {
-                usuarioAreaInstructiva3: row.get('usuarioAreaInstructiva3'),
-                documento_RSG: {
-                    nombre: row.get('nombre_RSG3'),
-                    path: row.get('documento_RSG'),
-                },
-                AR3_createdAt: row.get('RSG3_createdAt'),
+                numero_RSG: row.get('numero_RSG'),
+                fecha_RSG: row.get('fecha_RSG'),
+                AR3_createdAt: row.get('fecha_notificacion_RSG'),
             } : undefined,
             etapaDescargoRSG: row.get('usuarioAnalista4') ? {
-                usuarioAnalista4: row.get('usuarioAnalista4'),
-                documento_DRSG: {
-                    nombre: row.get('nombre_DRSG'),
-                    path: row.get('documento_DRSG'),
-                },
-                analista4_createdAt: row.get('DRSG_createdAt'),
+                nro_RSG: row.get('nro_RSG'),
+                fecha_RSG: row.get('fecha_RSG'),
             } : undefined,
             etapaRG: row.get('usuarioGerencia') ? {
-                usuarioGerencia: row.get('usuarioGerencia'),
-                documento_RG: {
-                    nombre: row.get('nombre_RG'),
-                    path: row.get('documento_RG'),
-                },
-                analista4_createdAt: row.get('RG_createdAt'),
+                numero_RG: row.get('numero_RG'),
+                fecha_RG: row.get('fecha_RG'),
+                fecha_notificacion_RG: row.get('fecha_notificacion_RG'),
             } : undefined,
             etapaConsentimiento: row.get('usuarioAnalista5') ? {
                 usuarioAnalista5: row.get('usuarioAnalista5'),
